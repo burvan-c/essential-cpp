09bf96f69f3a3fe225866894ad7296855b5050ed
2447146432 <2447146432@qq.com>
2022-05-06 19:23:26 +0800

    essential cpp

---
+++ chapter1/train1/chapter1.cpp
@@ -0,0 +1,24 @@
+//how to write a c++ program
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<string>
+using namespace std;//using and namespace are keywords,std为命名空间，
+int main()//程序从main 函数执行,int为keyword
+{
+	string user_name;
+	cout << "please enter your first name:";
+	cin >> user_name;
+	cout << "\nhello," << user_name << "……and goodbay!\n";
+
+	return 0;//程序代码所放位置
+}
+//function includes 函数名称、参数列表、函数体、返回类型
+//参数列表位于括号内，用逗号隔开各个类型
+//函数体在大括号内，有分号隔开
+//c面向过程，c++面向对象
+//类（class)指用户自定义的数据类型
+//基础数据类型：布尔值，字符，整数，浮点数
+//class分为头文件和程序执行文件
+//c++标准程序库为iostream,即调用#include<iostream>
+
+
